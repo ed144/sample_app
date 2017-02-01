ac689b62365cff0663a8be84e23b4c61c139dbe0
edik <ed1@nm.ru>
2017-02-01 13:05:30 +0300

    Add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe "StaticPages", type: :request do
 
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
@@ -10,10 +12,17 @@ RSpec.describe "StaticPages", type: :request do
        expect(response.body).to match "Sample App"
     end
 
-    it "should have the title 'Home'" do
+    it "should have the base title" do
       get '/static_pages/home'
-      expect(response.body).to include('<title>Ruby on Rails Tutorial Sample App | Home</title>')
+      expect(response.body).to include("<title>#{base_title}")
     end
+
+    it "should not have a custom page title" do
+      get '/static_pages/home'
+      expect(response.body).not_to include('| Home')
+    end
+
+
  
   end
 
@@ -25,7 +34,7 @@ RSpec.describe "StaticPages", type: :request do
 
     it "should have the title 'Help'" do
       get '/static_pages/help'
-      expect(response.body).to include('<title>Ruby on Rails Tutorial Sample App | Help</title>')
+      expect(response.body).to include("<title>#{base_title} | Help</title>")
     end
  
   end
@@ -38,9 +47,25 @@ RSpec.describe "StaticPages", type: :request do
 
     it "should have the title 'About Us'" do
       get '/static_pages/about'
-      expect(response.body).to include('<title>Ruby on Rails Tutorial Sample App | About Us</title>')
+      expect(response.body).to include("<title>#{base_title} | About Us</title>")
     end
 
   end 
+
+
+ describe "Contact page" do
+    it "should have the content 'Contact'" do
+      get '/static_pages/contact'
+      expect(response.body).to match "Contact"
+    end
+
+    it "should have the title 'Contact'" do
+      get '/static_pages/contact'
+      expect(response.body).to include("<title>#{base_title} | Contact</title>")
+    end
+
+  end 
+
+
 
 end
